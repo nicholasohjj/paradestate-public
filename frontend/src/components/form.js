98d531174033b2e0690c1fe472a9a07dda297d51
nicholasohjj <60575384+nicholasohjj@users.noreply.github.com>
2021-09-05 10:11:00 +0800

    uibuild

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -16,37 +16,42 @@ const Form = ({addName,newName,handleNameChange,newStatus,handleStatusChange}) =
     return (
       <div>
         <form onSubmit={addName}>
-        <TextField
-          id="standard-multiline-flexible"
-          label="Rank/Name"
-          multiline
-          maxRows={4}
-          value={newName}
-          onChange={handleNameChange}
-        />  
+          <div>
+            <TextField
+              id="standard-multiline-flexible"
+              label="Rank/Name"
+              multiline
+              maxRows={4}
+              value={newName}
+              onChange={handleNameChange}
+              />  
+          </div>
 
-        <FormControl className={useStyles().formControl}>
-        <InputLabel id="demo-simple-select-label">Current Status</InputLabel>
-        <Select
-          labelId="status"
-          id="status"
-          value ={newStatus}
-          onChange = {handleStatusChange}
-        >
-          <MenuItem value='present'>Present</MenuItem>
-          <MenuItem value='mc'>MC</MenuItem>
-          <MenuItem value='ma'>MA</MenuItem>
-          <MenuItem value='off'>Off</MenuItem>
-          <MenuItem value='leave'>Leave</MenuItem>
-          <MenuItem value='attached'>Attached Out</MenuItem>
-          <MenuItem value='others'>Others</MenuItem>
-          <MenuItem value='stayout'>Stay Out</MenuItem>
-        </Select>
-      </FormControl>
+          <div>
+            <FormControl className={useStyles().formControl}>
+              <InputLabel id="demo-simple-select-label">Current Status</InputLabel>
+              <Select
+              labelId="status"
+              id="status"
+              value ={newStatus}
+              onChange = {handleStatusChange}
+              >
+              <MenuItem value='present'>Present</MenuItem>
+              <MenuItem value='mc'>MC</MenuItem>
+              <MenuItem value='ma'>MA</MenuItem>
+              <MenuItem value='off'>Off</MenuItem>
+              <MenuItem value='leave'>Leave</MenuItem>
+              <MenuItem value='attached'>Attached Out</MenuItem>
+              <MenuItem value='others'>Others</MenuItem>
+              <MenuItem value='stayout'>Stay Out</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
 
-        <div>
-        <Button variant="contained" color="primary" type="submit">Add</Button>
-        </div>
+          <div>
+            <Button variant="contained" color="primary" type="submit">Add</Button>
+          </div>
+          
         </form>
       </div>
     )
