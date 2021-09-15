2b32e7e7735341a4dd021edbba87b7be8c2fb2e3
nicholasohjj <60575384+nicholasohjj@users.noreply.github.com>
2021-09-15 19:24:11 +0800

    uibuild

--- frontend/src/components/personnellist/updateButtons.js
+++ frontend/src/components/personnellist/updateButtons.js
@@ -11,7 +11,7 @@ const Updatebuttons = ({person, id, setPersons, persons}) => {
   const [show, setShow] = useState(false)
 
   const HandleDelete = () => {
-    if (window.confirm(`Do you want to delete ${person.name}`)) {
+    if (window.confirm(`Are you sure you want to permanently delete ${person.name}?`)) {
       phoneservice
         .removePerson(id)
         setPersons(persons.filter(person=>person.id !==id))
