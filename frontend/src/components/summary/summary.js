1527ba76aa1660760573684a0285570927c0744d
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:17:57 +0800

    Update summary.js

--- frontend/src/components/summary/summary.js
+++ frontend/src/components/summary/summary.js
@@ -1,36 +1,32 @@
 import React from "react";
 import Absentees from "./absentees";
 import Roles from "./roles";
-import {Text} from "atomize";
+import { Text } from "atomize";
 
 const style = {
-    padding: 0,
-    margin: 0
-  }
+  padding: 0,
+  margin: 0
+};
 
-const Summary = ({persons,
-                newcdo,
-                newcds,
-                newcos,
-                }) => {
-    let currentstrength = 0
-    persons.map(person=> (person.status==='present') ? (currentstrength = currentstrength+1) : currentstrength)
-    const totalstrength = persons.length
+const Summary = ({ persons, newcdo, newcds, newcos }) => {
+  const currentStrength = persons.filter(person => person.status === 'present').length;
+  const totalStrength = persons.length;
 
-    return (
-        <div>
-        <Text style={style}>CDO: {newcdo.toUpperCase()}</Text>
-        <Text style={style}>CDS: {newcds.toUpperCase()}</Text>
-        <Text style={style}>COS: {newcos.toUpperCase()}</Text>
-        <br/>
-        <Text style={style}>Total Strength: {totalstrength}</Text>
-        <Text style={style}>Present Strength: {currentstrength}</Text> 
-        <br/>
-        <Roles persons ={persons} />
-        <Absentees persons={persons} />
-        <Text style={style}>PRESENT IN CAMP: {currentstrength}</Text>
-        <br/>
-        </div>
-    )
-}
-export default Summary
+  return (
+    <div>
+      <Text style={style}>CDO: {newcdo.toUpperCase()}</Text>
+      <Text style={style}>CDS: {newcds.toUpperCase()}</Text>
+      <Text style={style}>COS: {newcos.toUpperCase()}</Text>
+      <br/>
+      <Text style={style}>Total Strength: {totalStrength}</Text>
+      <Text style={style}>Present Strength: {currentStrength}</Text> 
+      <br/>
+      <Roles persons={persons} />
+      <Absentees persons={persons} />
+      <Text style={style}>PRESENT IN CAMP: {currentStrength}</Text>
+      <br/>
+    </div>
+  );
+};
+
+export default Summary;
