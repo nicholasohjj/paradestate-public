e7b7bb42cdb6dc3bab500312021dddeb8c2eb9cc
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:11:26 +0800

    Update hq.js

--- frontend/src/components/contents/hq.js
+++ frontend/src/components/contents/hq.js
@@ -1,37 +1,37 @@
-import React from "react"
-import Emojidisplay from "../reusable/emoji"
-import Reason from "../reason"
+import React from 'react';
+import Emojidisplay from '../reusable/emoji';
+import Reason from '../reason';
 
 const style = {
-    padding: 0,
-    margin: 0
-  }
+  padding: 0,
+  margin: 0
+};
 
-const HQ = ({persons}) => {
-    
-    const HQList = persons.filter(person=> (String(person.group)).toLowerCase()==='hq') 
-    console.log(`List of HQ personnel: ${JSON.stringify(HQList)}`)
-    let currentstrength = 0
-    HQList.map(person=> (person.status==='present') ? (currentstrength = currentstrength+1) : currentstrength)
-    const totalstrength = HQList.length
-    return (
-        <div>
-          <p style={style}>HQ: {currentstrength}/{totalstrength}</p>
-            {HQList.map((person, index)=> 
-              <p style={style} key={person.name}>
-                {index+1}.  
-                {' ' + person.name}
-                <Emojidisplay currentStatus ={person.status} />
-                <Reason person = {person} />
+const HQ = ({ persons }) => {
+  const HQList = persons.filter(person => String(person.group).toLowerCase() === 'hq');
+  console.log(`List of HQ personnel: ${JSON.stringify(HQList)}`);
 
-              </p>
-                        
-            )}   
-        </div>
-      )
-}
+  let currentStrength = 0;
+  HQList.forEach(person => {
+    if (person.status === 'present') {
+      currentStrength++;
+    }
+  });
 
+  const totalStrength = HQList.length;
 
-  
+  return (
+    <div>
+      <p style={style}>HQ: {currentStrength}/{totalStrength}</p>
+      {HQList.map((person, index) => (
+        <p style={style} key={person.name}>
+          {index + 1}. {person.name}
+          <Emojidisplay currentStatus={person.status} />
+          <Reason person={person} />
+        </p>
+      ))}
+    </div>
+  );
+};
 
-  export default HQ
+export default HQ;
