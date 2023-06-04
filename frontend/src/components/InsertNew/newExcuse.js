18e591b4d26355084b32c1b1ae50add9b613fe27
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:07:06 +0800

    Update newExcuse.js

--- frontend/src/components/InsertNew/newExcuse.js
+++ frontend/src/components/InsertNew/newExcuse.js
@@ -1,37 +1,35 @@
-import React from "react";
-import { Input, Icon, Div } from "atomize";
+import React from 'react';
+import { Input, Icon, Div } from 'atomize';
 
-const NewExcuse = ({newexcuse,setnewExcuse}) => {
-
-    const handleExcuseChange = (event) => {
-        console.log(`Excuse: ${event.target.getAttribute("value")}`)
-          setnewExcuse(event.target.value)
-        }
+const NewExcuse = ({ newExcuse, setNewExcuse }) => {
+  const handleExcuseChange = (event) => {
+    console.log(`Excuse: ${event.target.value}`);
+    setNewExcuse(event.target.value);
+  };
 
   return (
-    <Div d='flex' justify={{ xs: "space-around", lg: "center" }}>
-    <Input
-    rounded="xl"
-    value={newexcuse}
-    onChange={handleExcuseChange}
-    placeholder="Medical Excuses (Optional)"
-    p={{ x: "2.5rem" }}
-    prefix={
-    <Icon
-    name="RemoveSolid"
-    color="danger800"
-    size="16px"
-    cursor="pointer"
-    pos="absolute"
-    top="50%"
-    left="0.75rem"
-    transform="translateY(-50%)"
-        />
-      }
-    />
-      </Div>
-    
+    <Div d="flex" justify={{ xs: 'space-around', lg: 'center' }}>
+      <Input
+        rounded="xl"
+        value={newExcuse}
+        onChange={handleExcuseChange}
+        placeholder="Medical Excuses (Optional)"
+        p={{ x: '2.5rem' }}
+        prefix={
+          <Icon
+            name="RemoveSolid"
+            color="danger800"
+            size="16px"
+            cursor="pointer"
+            pos="absolute"
+            top="50%"
+            left="0.75rem"
+            transform="translateY(-50%)"
+          />
+        }
+      />
+    </Div>
   );
-}
+};
 
-export default NewExcuse
+export default NewExcuse;
