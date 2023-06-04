dac98509fdff146d1b1909f5f362351b06261386
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:14:54 +0800

    Update title.js

--- frontend/src/components/title/title.js
+++ frontend/src/components/title/title.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React from "react";
+import { Div, Text, Image } from "atomize";
 
-//For future expansion
+// For future expansion: Replace the placeholder with your actual logo image source
+const logoSrc = "logo.png";
+
 const Title = () => {
-    return (
-        //Insert logo below
-        <div>
-        <h2>Parade State Application (For Internal Use Only)</h2>
-        </div>
-    )
-}
+  return (
+    <Div d="flex" align="center" justify="center" p="1rem">
+      <Image src={logoSrc} alt="Logo" h="3rem" mr="1rem" />
+      <Text tag="h2" textSize="xl" textColor="black500" textWeight="500">
+        Parade State Application (For Internal Use Only)
+      </Text>
+    </Div>
+  );
+};
 
-
-export default Title
+export default Title;
