7c57ba93aae992ba0944e3ea29d6be92fe1b010b
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:10:47 +0800

    Update paradestate.js

--- frontend/src/components/contents/paradestate.js
+++ frontend/src/components/contents/paradestate.js
@@ -1,29 +1,27 @@
-import React from 'react'
-import HQ from './hq' 
+import React from 'react';
+import HQ from './hq';
 import Blueteam from './Blueteam';
-import Redteam from './Redteam'
-import Excuses from './Excuses'
-import Signature from "./signature"
+import Redteam from './Redteam';
+import Excuses from './Excuses';
+import Signature from './signature';
 import Others from './Others';
 
-const ParadeState = ({persons, newauthor}) => {
+const ParadeState = ({ persons, newauthor }) => {
   return (
     <>
-    <HQ persons={persons}/>
-    <br/>
-    <Blueteam persons={persons}/>
-    <br/>
-    <Redteam persons={persons}/>
-    <br/>
-    <Others persons={persons} />
-    <br/>
-    <Excuses persons={persons}/>
-    <br/>
-    <Signature newauthor={newauthor}/> 
-    
+      <HQ persons={persons} />
+
+      <Blueteam persons={persons} />
+
+      <Redteam persons={persons} />
+
+      <Others persons={persons} />
+
+      <Excuses persons={persons} />
+
+      <Signature newauthor={newauthor} />
     </>
+  );
+};
 
-  )
-  }
-
-  export default ParadeState
+export default ParadeState;
