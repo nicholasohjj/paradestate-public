9fb73a0555ce5353b12020acd04729fab90de5d3
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:07:42 +0800

    Update newRoledropdown.js

--- frontend/src/components/InsertNew/newRoledropdown.js
+++ frontend/src/components/InsertNew/newRoledropdown.js
@@ -1,57 +1,54 @@
-import React, {useState} from "react";
-import {Icon, Div, Dropdown, Anchor} from "atomize";
+import React, { useState } from 'react';
+import { Icon, Div, Dropdown, Anchor } from 'atomize';
 
+const NewRoleDropDown = ({ setNewRole, newRole }) => {
+  const handleUpdateRole = (event) => {
+    setshowModal(!showModal);
+    console.log(`Role: ${event.target.getAttribute('value')}`);
+    setNewRole(event.target.getAttribute('value'));
+  };
 
+  const menuList = (
+    <Div p={{ x: '1rem', y: '0.5rem' }} value={newRole} onClick={handleUpdateRole}>
+      <Anchor d="block" p={{ y: '0.25rem' }} value="hq">
+        {'Coy HQ'}
+      </Anchor>
+      <Anchor d="block" p={{ y: '0.25rem' }} value="specs">
+        {'Specialist'}
+      </Anchor>
+      <Anchor d="block" p={{ y: '0.25rem' }} value="opr">
+        {'Operator'}
+      </Anchor>
+      <Anchor d="block" p={{ y: '0.25rem' }} value="admin">
+        {'Administrator'}
+      </Anchor>
+    </Div>
+  );
 
-const NewRoleDropDown = ({setnewRole, newRole}) => {
+  const [showModal, setshowModal] = useState(false);
 
-    const HandleUpdateRole = (event) => {
-      setshowModal(!showModal)
-      console.log(`Role: ${event.target.getAttribute("value")}`)
-      setnewRole(event.target.getAttribute("value"))
-          }
-    
-    const menuList = (
-      <Div p={{ x: "1rem", y: "0.5rem" }} value ={newRole} onClick ={HandleUpdateRole}>
-        <Anchor d="block" p={{ y: "0.25rem" }} value='hq' >
-            {"Coy HQ"}  
-        </Anchor>
-        <Anchor d="block" p={{ y: "0.25rem" }} value='specs' >
-            {"Specialist"}
-        </Anchor>
-        <Anchor d="block" p={{ y: "0.25rem" }} value='opr'>
-            {"Operator"}
-        </Anchor>
-        <Anchor d="block" p={{ y: "0.25rem" }} value='admin'>
-            {"Administrator"}
-        </Anchor>
-      </Div>
-    );
+  const handleClick = () => {
+    setshowModal(!showModal);
+  };
 
-    const [showModal, setshowModal] = useState(false)
-
-    const Handleclick = () => {
-        setshowModal(!showModal)
-    }
-    
-    return (
-        <Dropdown
-          rounded="xl" 
-          isOpen={showModal}
-          onClick={Handleclick}
-          menu={menuList}
-          bg="info100"
-        focusBg="info200"
-        borderColor="info600"
-        focusBorderColor="info800"
-        textColor="info800"
-        textWeight="500"
-        openSuffix={<Icon name="Up" color="info700" size="16px" />}
-        closeSuffix={<Icon name="Down" color="info700" size="16px" />}
-        >
-          {(newRole)? `Role: ${newRole.toUpperCase()}`: "Role"}
-        </Dropdown>
-      );
-}
+  return (
+    <Dropdown
+      rounded="xl"
+      isOpen={showModal}
+      onClick={handleClick}
+      menu={menuList}
+      bg="info100"
+      focusBg="info200"
+      borderColor="info600"
+      focusBorderColor="info800"
+      textColor="info800"
+      textWeight="500"
+      openSuffix={<Icon name="Up" color="info700" size="16px" />}
+      closeSuffix={<Icon name="Down" color="info700" size="16px" />}
+    >
+      {newRole ? `Role: ${newRole.toUpperCase()}` : 'Role'}
+    </Dropdown>
+  );
+};
 
 export default NewRoleDropDown;
