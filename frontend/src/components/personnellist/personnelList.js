774539707ded432b19de1f7f7754d77fef0ab1d1
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:31:30 +0800

    Update personnelList.js

--- frontend/src/components/personnellist/personnelList.js
+++ frontend/src/components/personnellist/personnelList.js
@@ -1,101 +1,75 @@
-import React, {useState} from 'react'
-import Updatebuttons from './updateButtons'; 
+import React, { useState } from 'react';
+import Updatebuttons from './updateButtons';
 import Emojidisplay from '../reusable/emoji';
 import Reason from '../reason';
 import { Div, Button, Icon } from "atomize";
 import Filter from './filter';
 
+const Personnellist = ({ setPersons, persons }) => {
+  const [show, setShow] = useState(false);
+  const [newFilter, setFilter] = useState('');
 
+  const handleShow = () => setShow(!show);
 
-const Personnellist = ({setPersons, persons}) => {
-  const [show, setShow] = useState(false)
-  const [ newFilter, setFilter ] = useState('')
+  const handleNameFilter = (event) => {
+    setFilter(event.target.value);
+  };
 
-  const HandleShow = () => setShow(!show)
+  const personnelToShow = !newFilter.trim()
+    ? persons
+    : persons.filter((person) =>
+        person.name.toLowerCase().includes(newFilter.toLowerCase())
+      );
 
-  const handleNamefilter = (event) => {
-    console.log(event.target.value)
-    setFilter(event.target.value)
-    }
-
-  const personnelToShow = (!newFilter.trim())
-  ? persons
-  : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
-
-  if (show===false) {
-    return (
-      <>
-                <Div d="flex" justify={{ xs: "space-around", lg: "center" }}>
-            <Button
-            textColor="black"
-            rounded="xl"
-              bg="info700"
-              hoverBg="info600"
-              m={{ r: "0.5rem",y: "0.5rem" }}
-              p={{ r: "1.5rem", l: "1rem" }}
-              shadow="3"
-              hoverShadow="4"
-              onClick={HandleShow}
-              suffix={
-                <Icon
-                name="EditSolid"
-                size="16px"
-                color="black"
-                m={{ l: "1rem" }}
-                
-              />} 
-              >
-                Edit
-            </Button>
-    </Div>
-      </>
-    )} else {
-      return (
-        <>
+  return (
+    <>
       <Div d="flex" justify={{ xs: "space-around", lg: "center" }}>
-            <Button
-            textColor="black"
-            rounded="xl"
-              bg="info700"
-              hoverBg="info600"
-              m={{ r: "0.5rem",y: "0.5rem" }}
-              p={{ r: "1.5rem", l: "1rem" }}
-              shadow="3"
-              hoverShadow="4"
-              onClick={HandleShow}
-              suffix={
-                <Icon
-                name="EditSolid"
-                size="16px"
-                color="black"
-                m={{ l: "1rem" }}
-                
-              />} 
-              >
-                Hide
-            </Button>
-    </Div>
-    <Filter newFilter={newFilter} handleNamefilter={handleNamefilter}/>
+        <Button
+          textColor="black"
+          rounded="xl"
+          bg="info700"
+          hoverBg="info600"
+          m={{ r: "0.5rem", y: "0.5rem" }}
+          p={{ r: "1.5rem", l: "1rem" }}
+          shadow="3"
+          hoverShadow="4"
+          onClick={handleShow}
+          suffix={
+            <Icon
+              name={show ? "EditSolid" : "HideSolid"}
+              size="16px"
+              color="black"
+              m={{ l: "1rem" }}
+            />
+          }
+        >
+          {show ? "Hide" : "Edit"}
+        </Button>
+      </Div>
+
+      {show && (
+        <>
+          <Filter newFilter={newFilter} handleNamefilter={handleNameFilter} />
           <ol>
-            {personnelToShow.map(person=>
+            {personnelToShow.map((person) => (
               <li key={person.name}>
                 {person.name}
-                <Emojidisplay currentStatus ={person.status} />
-                <Reason person = {person} />
-                <br/>
-                <Updatebuttons person={person}
-                               id={person.id}
-                               setPersons={setPersons}
-                               persons={persons}/>
+                <Emojidisplay currentStatus={person.status} />
+                <Reason person={person} />
+                <br />
+                <Updatebuttons
+                  person={person}
+                  id={person.id}
+                  setPersons={setPersons}
+                  persons={persons}
+                />
               </li>
-            )}
+            ))}
           </ol>
         </>
-      )
-  }
-  
+      )}
+    </>
+  );
+};
 
-    
-  }
-
-  export default Personnellist
+export default Personnellist;
