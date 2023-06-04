764cd204ea4eebd29066d6d347e8100af06fdd9b
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:09:31 +0800

    Update Blueteam.js

--- frontend/src/components/contents/Blueteam.js
+++ frontend/src/components/contents/Blueteam.js
@@ -1,36 +1,36 @@
-import React from "react"
-import Emojidisplay from "../reusable/emoji"
-import Reason from "../reason"
-import { Text } from "atomize"
+import React from 'react';
+import Emojidisplay from '../reusable/emoji';
+import Reason from '../reason';
+import { Text } from 'atomize';
+
 const style = {
-    padding: 0,
-    margin: 0
-  }
+  padding: 0,
+  margin: 0
+};
 
-const Blueteam = ({persons}) => {
-    
-    const BlueteamList = persons.filter(person=> (String(person.group)).toLowerCase()==='blueteam') 
-    console.log(`List of Blue Team members: ${JSON.stringify(BlueteamList)}`)
-    let currentstrength = 0
-    BlueteamList.map(person=> (person.status==='present') ? (currentstrength = currentstrength+1) : currentstrength)
-    const totalstrength = BlueteamList.length
-    return (
-        <div>
-          <p style={style}>BLUE TEAM: {currentstrength}/{totalstrength}</p>
-            {BlueteamList.map((person, index)=> 
-              <Text style={style} key={person.name}>
-                {index+1} .  
-                {' ' + person.name}
-                <Emojidisplay currentStatus ={person.status} />
-                <Reason person = {person} />
-              </Text>
-                        
-            )}   
-        </div>
-      )
-}
+const Blueteam = ({ persons }) => {
+  const BlueteamList = persons.filter((person) => String(person.group).toLowerCase() === 'blueteam');
+  console.log(`List of Blue Team members: ${JSON.stringify(BlueteamList)}`);
+  let currentStrength = 0;
+  BlueteamList.forEach((person) => {
+    if (person.status === 'present') {
+      currentStrength++;
+    }
+  });
+  const totalStrength = BlueteamList.length;
 
+  return (
+    <div>
+      <p style={style}>BLUE TEAM: {currentStrength}/{totalStrength}</p>
+      {BlueteamList.map((person, index) => (
+        <Text style={style} key={person.name}>
+          {index + 1}. {person.name}
+          <Emojidisplay currentStatus={person.status} />
+          <Reason person={person} />
+        </Text>
+      ))}
+    </div>
+  );
+};
 
-  
-
-  export default Blueteam 
+export default Blueteam;
