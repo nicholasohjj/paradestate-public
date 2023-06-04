2cc9cd19f51116f439ee4efea2ce363208c429ca
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:23:07 +0800

    Update emoji.js

--- frontend/src/components/reusable/emoji.js
+++ frontend/src/components/reusable/emoji.js
@@ -1,39 +1,33 @@
 import React from "react";
 
-const Emoji = ({symbol}) => (
-    <span
-        className="emoji"
-        role="img"
-    >
-        {symbol}
-    </span>
-  );
-  
-  const Emojidisplay = ({currentStatus}) => {
-    if (currentStatus==='present') {
-      return <Emoji symbol="✅"/>
-    } else if (currentStatus==='attached') {
-      return <Emoji symbol="🅰"/>
-    } else if (currentStatus==='others') {
-      return <Emoji symbol="🗒"/>
-    } else if (currentStatus==='mc') {
-    return <Emoji symbol="😷"/>
-    } else if (currentStatus==='ma') {
-      return <Emoji symbol="👨🏻‍⚕️"/>
-    } else if (currentStatus==='off') {
-    return <Emoji symbol="🚫"/>
-    } else if (currentStatus==='leave') {
-      return <Emoji symbol="🚪"/>
-    } else if (currentStatus==='leave') {
-    return <Emoji symbol="🚪"/>
-    } else if (currentStatus==='stayout') {
-    return <Emoji symbol="🏠"/>
-    } else if (currentStatus==='oncourse') {
-      return <Emoji symbol="👨🏻‍🎓"/> 
-    } else {
-    return ''
-    }
-  }
+const Emoji = ({ symbol }) => (
+  <span className="emoji" role="img">
+    {symbol}
+  </span>
+);
 
-export default Emojidisplay
-  
+const Emojidisplay = ({ currentStatus }) => {
+  if (currentStatus === "present") {
+    return <Emoji symbol="✅" />;
+  } else if (currentStatus === "attached") {
+    return <Emoji symbol="🅰" />;
+  } else if (currentStatus === "others") {
+    return <Emoji symbol="🗒" />;
+  } else if (currentStatus === "mc") {
+    return <Emoji symbol="😷" />;
+  } else if (currentStatus === "ma") {
+    return <Emoji symbol="👨🏻‍⚕️" />;
+  } else if (currentStatus === "off") {
+    return <Emoji symbol="🚫" />;
+  } else if (currentStatus === "leave") {
+    return <Emoji symbol="🚪" />;
+  } else if (currentStatus === "stayout") {
+    return <Emoji symbol="🏠" />;
+  } else if (currentStatus === "oncourse") {
+    return <Emoji symbol="👨🏻‍🎓" />;
+  } else {
+    return "";
+  }
+};
+
+export default Emojidisplay;
