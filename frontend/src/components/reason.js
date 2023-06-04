568e841a3d0140db65110d475c73257b4f80a923
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:13:09 +0800

    Update reason.js

--- frontend/src/components/reason.js
+++ frontend/src/components/reason.js
@@ -1,18 +1,11 @@
 import React from "react";
 
-const Reason = ({person}) => {
-    if (person.reason  && (person.reason!=='nil')) {
-        return (
-            <>
-            ({person.reason})
-            </>
-        )
-    } else {
-        return (
-            <>
-            </>
-        )
-    }
-}
+const Reason = ({ person }) => {
+  if (person.reason && person.reason !== 'nil') {
+    return <>({person.reason})</>;
+  } else {
+    return null;
+  }
+};
 
-export default Reason
+export default Reason;
