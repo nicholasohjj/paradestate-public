315205054e5e8c03c06c355b139193c5cf96e52b
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:12:47 +0800

    Update Excuses.js

--- frontend/src/components/contents/Excuses.js
+++ frontend/src/components/contents/Excuses.js
@@ -1,47 +1,33 @@
-import React from "react"
+import React from "react";
 
 const style = {
-    padding: 0,
-    margin: 0
+  padding: 0,
+  margin: 0
+};
+
+const Excuse = ({ person }) => {
+  if (person.excuse && person.excuse !== 'nil') {
+    return <>({person.excuse})</>;
+  } else {
+    return null;
   }
+};
 
-const Excuse = ({person}) => {
-  if (person.excuse) {
-    return (
-        <>
-        ({person.excuse})
-        </>
-    )
-} else {
-    return (
-        <>
-        </>
-    )
-}
-}
+const Excuses = ({ persons }) => {
+  const ExcuseList = persons.filter(person => person.excuse && person.excuse !== 'nil');
+  console.log(`List of personnel with excuses: ${JSON.stringify(ExcuseList)}`);
+  const currentStrength = ExcuseList.length;
 
-const Excuses = ({persons}) => {
-  
-    
-    const ExcuseList = persons.filter(person=> person.excuse && (person.excuse!=='nil'))
-    console.log(`List of personnel with excuses: ${JSON.stringify(ExcuseList)}` )
-    const currentstrength = ExcuseList.length
-    return (
-        <div>
-          <p style={style}>PERSONNEL WITH MEDICAL EXCUSES: {currentstrength}</p>
-            {ExcuseList.map((person, index)=> 
-              <p style={style} key={person.name}>
-                {index+1}.  
-                {' ' + person.name + ' '}
-                <Excuse person={person} />
-              </p>
-                        
-            )}   
-        </div>
-      )
-}
+  return (
+    <div>
+      <p style={style}>PERSONNEL WITH MEDICAL EXCUSES: {currentStrength}</p>
+      {ExcuseList.map((person, index) => (
+        <p style={style} key={person.name}>
+          {index + 1}. {person.name} <Excuse person={person} />
+        </p>
+      ))}
+    </div>
+  );
+};
 
-
-  
-
-  export default Excuses
+export default Excuses;
