82e89968907255a3fc9cbe1d6608f40eef5a1ff3
Nicholas Oh <nicholasohjj@gmail.com>
2023-06-04 14:11:53 +0800

    Update Redteam.js

--- frontend/src/components/contents/Redteam.js
+++ frontend/src/components/contents/Redteam.js
@@ -1,37 +1,38 @@
-import React from "react"
-import Emojidisplay from "../reusable/emoji"
-import Reason from "../reason"
-import { Text } from "atomize"
+import React from "react";
+import Emojidisplay from "../reusable/emoji";
+import Reason from "../reason";
+import { Text } from "atomize";
 
 const style = {
-    padding: 0,
-    margin: 0
-  }
+  padding: 0,
+  margin: 0
+};
 
-const Redteam = ({persons}) => {
-    
-    const RedteamList = persons.filter(person=> (String(person.group)).toLowerCase()==='redteam') 
-    console.log(`List of Red Team members: ${JSON.stringify(RedteamList)}`)
-    let currentstrength = 0
-    RedteamList.map(person=> (person.status==='present') ? (currentstrength = currentstrength+1) : currentstrength)
-    const totalstrength = RedteamList.length
-    return (
-        <div>
-          <p style={style}>RED TEAM: {currentstrength}/{totalstrength}</p>
-            {RedteamList.map((person, index)=> 
-              <Text style={style} key={person.name}>                {index+1}.  
-                {' ' + person.name}
-                <Emojidisplay currentStatus ={person.status} />
-                <Reason person = {person} />
+const Redteam = ({ persons }) => {
+  const RedteamList = persons.filter(person => String(person.group).toLowerCase() === 'redteam');
+  console.log(`List of Red Team members: ${JSON.stringify(RedteamList)}`);
 
-              </Text>
-                        
-            )}   
-        </div>
-      )
-}
+  let currentStrength = 0;
+  RedteamList.forEach(person => {
+    if (person.status === 'present') {
+      currentStrength++;
+    }
+  });
 
+  const totalStrength = RedteamList.length;
 
-  
+  return (
+    <div>
+      <Text style={style}>RED TEAM: {currentStrength}/{totalStrength}</Text>
+      {RedteamList.map((person, index) => (
+        <Text style={style} key={person.name}>
+          {index + 1}. {person.name}
+          <Emojidisplay currentStatus={person.status} />
+          <Reason person={person} />
+        </Text>
+      ))}
+    </div>
+  );
+};
 
-  export default Redteam
+export default Redteam;
